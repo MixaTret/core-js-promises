bc21f46227eabaec2bbfd70c90b4d477b5d3ed0c
MixaTret <mikhail.tretsiakevich@gmail.com>
2024-03-25 11:37:13 +0000

    feat: all tasks were done

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,16 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  return new Promise((resolve, reject) => {
+    if (number >= 0) {
+      resolve();
+    } else {
+      reject();
+    }
+  })
+    .then()
+    .catch();
 }
 
 /**
@@ -33,8 +41,16 @@ function getPromise(/* number */) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  return new Promise((resolve) => {
+    source
+      .then(() => {
+        resolve('success');
+      })
+      .catch(() => {
+        resolve('fail');
+      });
+  });
 }
 
 /**
@@ -50,8 +66,16 @@ function getPromiseResult(/* source */) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return new Promise((resolve, reject) => {
+    Promise.any(promises)
+      .then((result) => {
+        resolve(result);
+      })
+      .catch(() => {
+        reject();
+      });
+  });
 }
 
 /**
@@ -73,8 +97,16 @@ function getFirstResolvedPromiseResult(/* promises */) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promises) {
+  return new Promise((resolve, reject) => {
+    Promise.race(promises)
+      .then((result) => {
+        resolve(result);
+      })
+      .catch(() => {
+        reject();
+      });
+  });
 }
 
 /**
@@ -88,8 +120,16 @@ function getFirstPromiseResult(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return new Promise((resolve, reject) => {
+    Promise.all(promises)
+      .then((result) => {
+        resolve(result);
+      })
+      .catch((result) => {
+        reject(result);
+      });
+  });
 }
 
 /**
@@ -104,10 +144,13 @@ function getAllOrNothing(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((results) => {
+    return results.map((result) => {
+      return result.status === 'fulfilled' ? result.value : null;
+    });
+  });
 }
-
 /**
  * Takes an array of promises and processes them sequentially, concatenating each resolved value into a single string.
  * The resolution order is determined by the order of the promises in the array, not by their resolution time.
@@ -126,8 +169,21 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return new Promise((resolve) => {
+    const results = [];
+    let completed = 0;
+
+    promises.forEach((promise, index) => {
+      promise.then((result) => {
+        results[index] = result;
+        completed += 1;
+        if (completed === promises.length) {
+          resolve(results.join(''));
+        }
+      });
+    });
+  });
 }
 
 module.exports = {
